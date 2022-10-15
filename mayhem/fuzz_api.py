3422fe13c16cca9b4fe97ab6bf0dd4584c623dd1
ennamarie19 <dallmeyerm@att.net>
2022-10-15 14:36:13 -0500

    simplified

--- mayhem/fuzz_api.py
+++ mayhem/fuzz_api.py
@@ -3,38 +3,17 @@ import atheris
 import sys
 import io
 
-with atheris.instrument_imports(exclude=["subprocess", "urllib", "urllib.parse"]):
+with atheris.instrument_imports():
     import isort
 
-
-class SplitAwareFuzzedDataProvider(atheris.FuzzedDataProvider):
-    def __init__(self, data, split_count: int):
-        super(SplitAwareFuzzedDataProvider, self).__init__(data)
-        self._chunk_size = len(data) // split_count
-
-    def ConsumeByteSegment(self):
-        return self.ConsumeBytes(self._chunk_size)
-
-    def ConsumeStringSegment(self):
-        return self.ConsumeString(self._chunk_size)
-
-
-split_count = 4
 
 
 @atheris.instrument_func
 def TestOneInput(data):
-    fdp = SplitAwareFuzzedDataProvider(data, split_count)
 
     try:
-        isort.code(fdp.ConsumeStringSegment())
+        isort.code(str(data))
 
-        # text_io = io.StringIO(fdp.ConsumeStringSegment())
-        # isort.check_stream(text_io, True)
-        #
-        # isort.check_code(fdp.ConsumeStringSegment(), True)
-        #
-        # isort.find_imports_in_code(fdp.ConsumeStringSegment())
     except isort.exceptions.ISortError:
         pass
 
